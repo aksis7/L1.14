91ea06f5cdb14a826ac7c0e777b7583b94b3ffce
Marat <ban54894@gmail.com>
2024-12-10 20:42:17 +0500

    Initial commit for L1.14

---
+++ 14.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func determineType(value interface{}) {
+	switch v := value.(type) {
+	case int:
+		fmt.Println("Тип: int")
+	case string:
+		fmt.Println("Тип: string")
+	case bool:
+		fmt.Println("Тип: bool")
+	case chan any:
+		fmt.Println("Тип: channel")
+	default:
+		fmt.Println("Неизвестный тип:", v)
+	}
+}
+
+func main() {
+	var x interface{}
+
+	// Пример с типом int
+	x = 42
+	determineType(x)
+
+	// Пример с типом string
+	x = "Hello, Go!"
+	determineType(x)
+
+	// Пример с типом bool
+	x = true
+	determineType(x)
+
+	// Пример с типом channel
+	x = make(chan int)
+	determineType(x)
+
+	// Пример с неизвестным типом
+	x = 3.14
+	determineType(x)
+}
